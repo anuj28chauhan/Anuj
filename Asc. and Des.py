b6e3cd425aa869f2529123c9a883ff93f327a5bc
anuj28chauhan <98798340+anuj28chauhan@users.noreply.github.com>
2022-02-01 11:30:23 +0530

    Add files via upload

---
+++ Asc. and Des.py
@@ -0,0 +1,8 @@
+n = eval(input("Enter the list :: "))
+print("")
+n.sort()
+print("List in ascending order :: \n\n",n)
+print("")
+n.sort(reverse(True))
+print("List in descending order :: \n\n",n)
+
